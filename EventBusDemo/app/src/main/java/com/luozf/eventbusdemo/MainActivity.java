fd3bf3f20fa9fd77a49d02964bf5e0c328912924
luozf12345 <luozaifei1997@gmail.com>
2020-11-02 23:28:01 +0800

    提交eventBus学习与思考

--- EventBusDemo/app/src/main/java/com/luozf/eventbusdemo/MainActivity.java
+++ EventBusDemo/app/src/main/java/com/luozf/eventbusdemo/MainActivity.java
@@ -7,15 +7,20 @@ import android.widget.Toast;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.meta.SubscriberInfo;
+import org.greenrobot.eventbus.meta.SubscriberInfoIndex;
 
 /**
  * 1.builder创建者，门面模式，单例模式，public EventBus(DEFAULT_BUILDER)。
  * 2.方法职责分明
  * 3.CopyOnWriteArrayList，PendingPostQueue，同步锁尽量小的代码块
  * 5.HashMap缓存提高性能
- * 6.观察者优先级，线程模型，粘性
+ * 6.观察者优先级（处理函数的，不是事件的），线程模型，粘性
  * 7.ThreadLocal绑定线程变量
  * 8.为什么post时需要eventQueue？？
+ * 9.引用传递，可双向修改
+ * 10.既然有优先级，是否可以有序，消费掉事件？
+ * 11.index索引是啥？通过编译时生成索引，注册时先去索引里查找，索引没有，才通过反射获取方法。
  *
  * 原理：
  * 1.如何做的？
@@ -28,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         EventBus.getDefault().register(this);
+        EventBus.builder().addIndex(new SubscriberInfoIndex() {
+            @Override
+            public SubscriberInfo getSubscriberInfo(Class<?> subscriberClass) {
+                return null;
+            }
+        })
     }
 
     @Subscribe
